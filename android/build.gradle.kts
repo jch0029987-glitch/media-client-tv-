db96ed5dacb4972c4adf0a9846bb92621daf8e2d
jch0029987-glitch <jch0029987@gmail.com>
2026-09-15 21:53:51 -0500

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -11,16 +11,16 @@ val newBuildDir: Directory =
         .get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+subprojects.forEach { subproject ->
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(subproject.name)
+    subproject.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 
 // Force all library subprojects (like serious_python_android) to compile against SDK 36 safely
-subprojects {
-    afterEvaluate { project: org.gradle.api.Project ->
-        project.pluginManager.withPlugin("com.android.library") {
-            val androidExt = project.extensions.findByName("android")
+subprojects.forEach { subproject ->
+    subproject.afterEvaluate { proj ->
+        proj.pluginManager.withPlugin("com.android.library") {
+            val androidExt = proj.extensions.findByName("android")
             if (androidExt != null) {
                 try {
                     val getMethod = (androidExt as Any).javaClass.getMethod("getCompileSdk")
@@ -37,8 +37,8 @@ subprojects {
     }
 }
 
-subprojects {
-    project.evaluationDependsOn(":app")
+subprojects.forEach { subproject ->
+    subproject.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
