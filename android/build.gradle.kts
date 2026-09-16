32de9d4bfc915a47ca64f5f7da825a9f629f910e
jch0029987-glitch <jch0029987@gmail.com>
2026-09-15 21:46:31 -0500

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,6 +15,27 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
+// Force all subproject libraries (like serious_python_android) to compile against SDK 36
+subprojects {
+    afterEvaluate { p ->
+        p.plugins.withId("com.android.library") {
+            p.extensions.findByName("android")?.let { androidExt ->
+                try {
+                    val getMethod = androidExt.javaClass.getMethod("getCompileSdk")
+                    val setMethod = androidExt.javaClass.getMethod("setCompileSdk", Int::class.java)
+                    val currentSdk = getMethod.invoke(androidExt) as? Int
+                    if (currentSdk == null || currentSdk < 36) {
+                        setMethod.invoke(androidExt, 36)
+                    }
+                } catch (e: Exception) {
+                    // Ignored if reflection fails
+                }
+            }
+        }
+    }
+}
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
