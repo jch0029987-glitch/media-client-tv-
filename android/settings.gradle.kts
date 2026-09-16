27739dadd892ce5c0c1a79ab4546c5711c15cc89
jch0029987-glitch <jch0029987@gmail.com>
2026-09-15 21:41:46 -0500

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,3 +1,24 @@
+pluginManagement {
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        val file = file("local.properties")
+        if (file.exists()) {
+            properties.load(java.io.FileInputStream(file))
+        }
+        properties.getProperty("flutter.sdk") ?: System.getenv("FLUTTER_ROOT")
+    }
+    
+    if (flutterSdkPath != null) {
+        includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+    }
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.1.1" apply false
